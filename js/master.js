11d19c3cf51bb7b98643e5889e79dffbd185a5b1
tortul <austindobrusky@gmail.com>
2018-12-08 15:43:54 -0600

    Solid navbar base

---
+++ js/master.js
@@ -0,0 +1,8 @@
+//-------------------- ProjectA Dynamic Variables ---------------------------
+var navItemWidth = 80;
+var carouselInterval = 10000;
+var navDuration = 600;
+var navShrink = 2.5;
+var carouselDirection = 'right'; //Accepts right or left
+var carouselFade = 200;
+var dropSpeed = 400;

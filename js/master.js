4b26b66bd19de1dc425e18d722783ca5afc70585
tortul <austindobrusky@gmail.com>
2018-12-09 18:26:29 -0600

    Worked on langing page and changed navbar theme

--- js/master.js
+++ js/master.js
@@ -6,3 +6,15 @@ var navShrink = 2.5;
 var carouselDirection = 'right'; //Accepts right or left
 var carouselFade = 200;
 var dropSpeed = 400;
+
+//Sliding animation time
+
+
+//Dont mind the repetition of the images in the navchange im just lazy
+$(document).ready(function() {
+  navChange('#navchange', 'img/logo.png', 'img/logo.png');
+});
+
+$(document).scroll(function() {
+  navChange('#navchange', 'img/logo.png', 'img/logo.png');
+});

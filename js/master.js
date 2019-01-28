245e4364a914b811d0a90f771e9d7831a45493c3
thefierytortoise <austindobrusky@gmail.com>
2019-01-28 15:37:21 -0600

    calendar

--- js/master.js
+++ js/master.js
@@ -13,9 +13,9 @@ function fadeDown() {
 };
 
 //Calendar vars
-var monthName = ['January', 'February', 'March', 'April', 'May', 'June',
-'July', 'August', 'September', 'October', 'November', 'December'];
+var monthName = ['January', 'February', 'March', 'April'];
 var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var january = [' ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, ' ', ' '];
 var date = new Date();
 var m = date.getMonth();
 var today = date.getDay();
@@ -26,8 +26,8 @@ function monthChange(month, day, weekday) {
   $('.month').html(monthName[month]);
   for(var i = 0; i < 6; i++) {
     $('.days').append('<tr></tr>');
-    while(j < 32) {
-      $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + j + '</td>');
+    while(j < 36) {
+      $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + january[j-1] + '</td>');
       if((j % 7) === 0) {
         j++;
         break;
@@ -37,6 +37,13 @@ function monthChange(month, day, weekday) {
   }
 };
 
+function modal() {
+  $('.days > tbody > tr > td').click(function() {
+    $('.modal').fadeIn();
+  });
+}
+
 $(document).ready(function() {
   monthChange(m, date.getDate(), today);
+  modal();
 });

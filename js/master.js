7d2bc8b6144e09b97fe1cbb073218fdce3f276c0
thefierytortoise <austindobrusky@gmail.com>
2019-02-01 15:33:03 -0600

    Worked on modal

--- js/master.js
+++ js/master.js
@@ -15,19 +15,19 @@ function fadeDown() {
 //Calendar vars
 var monthName = ['January', 'February', 'March', 'April'];
 var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-var january = [' ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, ' ', ' '];
+var february = [ ' ', ' ', ' ', ' ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, ' ', ' '];
 var date = new Date();
 var m = date.getMonth();
 var today = date.getDay();
 var j = 1;
 
 //TODO: gonna make this super easy and just make arrays with numbers and have the table load certain arrays based on the month.
-function monthChange(month, day, weekday) {
+function monthChange(month) {
   $('.month').html(monthName[month]);
   for(var i = 0; i < 5; i++) {
     $('.days').append('<tr></tr>');
     while(j < 36) {
-      $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + january[j-1] + '</td>');
+      $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + february[j-1] + '</td>');
       if((j % 7) === 0) {
         j++;
         break;
@@ -40,10 +40,14 @@ function monthChange(month, day, weekday) {
 function modal() {
   $('.days > tbody > tr > td').click(function() {
     $('.modal').fadeIn();
+    $('body').css('overflow', 'hidden');
+  });
+  $('.modalform i').click(function() {
+     $('.modal').fadeOut();
   });
 }
 
 $(document).ready(function() {
-  monthChange(m, date.getDate(), today);
+  monthChange(m);
   modal();
 });

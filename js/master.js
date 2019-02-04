547be3dbe6db244763afa171b8476ffccd32a363
thefierytortoise <austindobrusky@gmail.com>
2019-02-04 15:36:30 -0600

    asasf

--- js/master.js
+++ js/master.js
@@ -12,22 +12,41 @@ function fadeDown() {
   $('.fade-down').css({'top':'0px','opacity':'1'});
 };
 
-//Calendar vars
-var monthName = ['January', 'February', 'March', 'April'];
+//Calendar vars this could be way better but lack of time
+var monthName = ['January', 'February', 'March', 'April', 'May'];
 var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 var february = [ ' ', ' ', ' ', ' ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, ' ', ' '];
+var march = [ ' ', ' ', ' ', ' ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+var april = [ ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, ' ', ' ', ' ', ' '];
+var may = [ ' ', '  ', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, ' ', ' '];
+
 var date = new Date();
 var m = date.getMonth();
 var today = date.getDay();
 var j = 1;
 
-//TODO: gonna make this super easy and just make arrays with numbers and have the table load certain arrays based on the month.
 function monthChange(month) {
   $('.month').html(monthName[month]);
   for(var i = 0; i < 5; i++) {
     $('.days').append('<tr></tr>');
     while(j < 36) {
-      $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + february[j-1] + '</td>');
+      switch(month) {
+        case 1:
+          $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + february[j-1] + '</td>');
+          break;
+
+        case 2:
+          $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + march[j-1] + '</td>');
+          break;
+
+        case 3:
+          $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + april[j-1] + '</td>');
+          break;
+
+        case 4:
+          $('.days tr:nth-child(' + (i + 2) + ')').append('<td>' + may[j-1] + '</td>');
+          break;
+      }
       if((j % 7) === 0) {
         j++;
         break;
@@ -45,7 +64,14 @@ function modal() {
   $('.modalform i').click(function() {
      $('.modal').fadeOut();
   });
-}
+};
+
+function schedule() {
+  $('.modalform').html('<p>Thank you for choosing Accuracy</p>');
+  setTimeout(function () {
+    $('.modal').fadeOut();
+  }, 2000);
+};
 
 $(document).ready(function() {
   monthChange(m);
